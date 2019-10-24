5df34714fd2441c7c2f45363278313d532e54c2a
Makayla-Skiff <54339097+Makayla-Skiff@users.noreply.github.com>
2019-10-24 12:59:18 -0500

    Add files via upload

---
+++ GrabData.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyCaneLane1._1.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandyCaneLane1._1.Pages
+{
+    public class GrabDataModel : PageModel
+    {
+        public ApplicationDbContext _db { get; set; }
+
+        public IList<Candies> Candies { get; set; }
+
+        public GrabDataModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Candies = await _db.Candies.ToListAsync();
+        }
+    }
+}
---
+++ Indiv.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CandyCaneLane1._1.Pages
+{
+    public class IndivModel : PageModel
+    {
+        public void OnGet(int? id)
+        {
+            System.Diagnostics.Debug.WriteLine(id);
+        }
+    }
+}
